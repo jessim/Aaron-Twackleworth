d71f0a9229a1d0f3e245b8b0c5dc172ef8ef09f7
admin <admin@1399cc02-af35-4323-bb37-a78714d85136>
2009-09-14 13:47:38 +0000

    delete - clear

--- Supemi/Supemi_v_0_93a2/sidebar-optional.php
+++
@@ -1,5 +0,0 @@
-<div id="optionalsidebarcss">
-<ul style="list-style: none;margin-left:-40px;">
-<?php if ( function_exists ( 'dynamic_sidebar' ) && dynamic_sidebar('sidebar-optional') ) : endif; ?>
-</ul>
-</div>
