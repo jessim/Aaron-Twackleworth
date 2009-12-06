f9fbc53becbedb540aebbdc1366b25eca9b62bf5
admin <admin@1399cc02-af35-4323-bb37-a78714d85136>
2009-12-06 12:42:39 +0000

    AABBOO V.0.93 RELEASED
    cleaned 3 off trunk

---
+++ Aabboo/tags/aabboo-v-0-93/constant2.php
@@ -0,0 +1,7 @@
+	<div id="bottom-container">	
+
+		<?php get_sidebar(); ?>
+
+	</div>
+
+</div>						
--- Aabboo/trunk/3/constant1.php
+++
@@ -1,9 +0,0 @@
-<div id="content-container">  	
-
-	<div id="top-container">  	
-
-		<div id="optional-sidebar">
-			
-			<?php include ('sidebar-optional.php'); ?>
-
-		</div>
